b9591e40024b504d9b62cb3cfae3a98bc61b8c08
zorainaalyne <zorainaalyne16@gmail.com>
2025-02-21 09:53:30 -0300

    afazeres

--- db.sql
+++ db.sql
@@ -1,1 +1,30 @@
- -- fu
+ -- Criação de Tabelas
+ 
+ --  Usuario (Jhuly)
+
+
+ --  Pomodoro(Jhuly)
+
+
+ -- AUTH (Jander)
+
+
+ -- Lista de Tarefas (Jander)
+
+
+-- Gamificação (Vinicius)
+
+
+-- Tarefa (Vinicius)
+
+
+-- Modo Foco (Edsandro)
+
+
+-- Cliente(Edsandro)
+
+
+-- Calendário (Zoraina)
+
+
+-- Notificação (Zoraina)
