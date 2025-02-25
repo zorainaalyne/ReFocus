03a47b94e10007f1413b5ece8e777fdacfe2a574
pgDEV69 <pedrofucapimidia@gmail.com>
2025-02-25 08:12:27 -0400

    ta fazido

--- db.sql
+++ db.sql
@@ -19,7 +19,13 @@
 
 
 -- Modo Foco (Edsandro)
-
+CREATE TABLE ModoFoco (
+    id SERIAL PRIMARY KEY,
+    usuarioId INTEGER NOT NULL,
+    ativo BOOLEAN DEFAULT FALSE,
+    tempoAtivo INTERVAL DEFAULT '00:00:00',
+    FOREIGN KEY (usuarioId) REFERENCES Usuario(id) ON DELETE CASCADE
+);
 
 -- Cliente(Edsandro)
 
